839a13e1a6f49137cbac56f901313e3bfd1e463a
Mmariami <mxarshiladze29@gmail.com>
2021-08-03 10:57:23 +0300

    task4 done

---
+++ task4/script.js
@@ -0,0 +1,29 @@
+const countTotalSalary = function(employees) {
+    let salaries = Object.values(employees)
+    let sum = 0;
+    for (let i = 0; i < salaries.length; i++) 
+    { sum += salaries[i]; }
+    return sum;
+};
+
+
+
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+// console.log(countTotalSalary({})); // 0
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
